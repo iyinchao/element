c3ab9b70be7d5d1b6de7ca8f0d9eefdac10f0d5c
charlieyin(尹超) <charlieyin@tencent.com>
2017-02-25 19:06:14 +0800

    Added comment for last commit.

--- src/utils/clickoutside.js
+++ src/utils/clickoutside.js
@@ -3,9 +3,14 @@ import { on } from 'element-ui/src/utils/dom';
 const nodeList = [];
 const ctx = '@@clickoutsideContext';
 
+/**
+ * 使用use capture来先期获取click事件，避免某些子元素或document的其他
+ * listener阻止事件传播使得该directive失效
+ */
 !Vue.prototype.$isServer && on(document, 'click', e => {
   nodeList.forEach(node => node[ctx].documentHandler(e));
 }, true);
+
 /**
  * v-clickoutside
  * @desc 点击元素外面才会触发的事件
